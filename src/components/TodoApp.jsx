18df0e58cdd255f82c2a33f31502e64fd36c392a
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-05 11:24:15 +0500

    To create and a few change clients side

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoApp = () => {
+  return (
+    <div className='bg-white rounded-lg w-9/12 mx-auto py-4 px-8 shadow-md'>
+      
+    </div>
+  )
+}
+
+export default TodoApp
