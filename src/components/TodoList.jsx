0f88fc7d39e82c5d73bf6b9fc08d29ebecd35480
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-07 23:44:18 +0500

    To create and a few change in component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import TodoItem from './TodoItem';
+import React from "react";
+import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, deleteTodo, toggleComplete }) => {
-    return (
-        <div className="md:w-6/12 mx-auto mt-6 space-y-3 overflow-y-auto">
-            {todos.length === 0 ? (
-                <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md">
-                    No tasks yet! Add a new item above.
-                </p>
-            ) : (
-                todos.map(todo => (
-                    <TodoItem 
-                        key={todo.id} 
-                        todo={todo} 
-                        deleteTodo={deleteTodo} 
-                        toggleComplete={toggleComplete} 
-                    />
-                ))
-            )}
-        </div>
-    );
+  return (
+    <div className="md:w-6/12 mx-auto mt-6 space-y-3 overflow-y-auto">
+      {todos.length === 0 ? (
+        <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md">
+          No tasks yet! Add a new item above.
+        </p>
+      ) : (
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            toggleComplete={toggleComplete}
+          />
+        ))
+      )}
+    </div>
+  );
 };
 
 export default TodoList;
