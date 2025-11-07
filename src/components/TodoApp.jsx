bff0ca35817f634c0356c0d3029bd63785951724
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-07 23:51:31 +0500

    To create and a few change in component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const TodoApp = () => {
   return (
     <div>
-      
+      <form action="">
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TodoApp
+export default TodoApp;
