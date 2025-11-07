63ee08a3f75cef5821f54135bcdf849b2c13df7b
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-07 23:50:25 +0500

    To create and a few change in component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const TodoItem = () => {
   return (
     <div>
-      
+      <form action="">
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
