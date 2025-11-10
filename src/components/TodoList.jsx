c74f375ca8b54999d2aec368cc545fffb445f25a
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-10 22:41:52 +0500

    Make a few changes form component and five component changes as well

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,6 +4,534 @@ import TodoItem from "./TodoItem";
 const TodoList = ({ todos, deleteTodo, toggleComplete }) => {
   return (
     <div className="md:w-6/12 mx-auto mt-6 space-y-3 overflow-y-auto">
+      <div>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+      <div>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+        <form action="">
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
       {todos.length === 0 ? (
         <p className="text-center text-gray-500 p-4 bg-white rounded-lg shadow-md">
           No tasks yet! Add a new item above.
